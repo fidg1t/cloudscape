43b3b974ea8fee6c13a5cebd67c4eed3bf44ee02
Nicholas Brennan <nicholasalexanderbrennan@gmail.com>
2025-07-30 16:25:46 -0700

    Added abstract System class to Cloudscape

---
+++ Cloudscape/include/System.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Cloudscape {
+
+  class CLAPI System
+  {
+  public:
+    // For derived classes
+    virtual ~System() = default;
+
+    // Pure virtual
+    virtual void Init() = 0;
+    virtual void Load() = 0;
+    virtual void Update(float dt) = 0;
+    virtual void Draw() = 0;
+    virtual void Exit() = 0;
+ 
+  };
+
+}
