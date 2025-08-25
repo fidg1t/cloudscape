881c444e0cf01db41107aa73c59e26d49d0892e3
NovaTheThird <nicholasalexanderbrennan@gmail.com>
2025-08-24 22:53:06 -0700

    Updated CLAPI for Unix Compatability

--- Cloudscape/include/Core.h
+++ Cloudscape/include/Core.h
@@ -1,5 +1,11 @@
-#ifdef CL_ENGINE
-	#define CLAPI __declspec(dllexport)
+#pragma once
+
+#ifdef _WIN32
+	#ifdef CL_ENGINE
+		#define CLAPI __declspec(dllexport)
+	#else
+		#define CLAPI __declspec(dllimport)
+	#endif
 #else
-	#define CLAPI __declspec(dllimport)
+	#define CLAPI
 #endif
