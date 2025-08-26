7604e5f5b6d19a419de0400f8a2be83afbf01ed8
NovaTheThird <nicholasalexanderbrennan@gmail.com>
2025-08-26 01:01:19 -0700

    Added 'logging' to Core.h

--- Cloudscape/include/Core.h
+++ Cloudscape/include/Core.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+
+// CLAPI Macro for Exporting Functions to DLL on Windows
+
 #ifdef _WIN32
 	#ifdef CL_ENGINE
 		#define CLAPI __declspec(dllexport)
@@ -8,4 +12,15 @@
 	#endif
 #else
 	#define CLAPI
+#endif
+
+///TODO: Add spdlog to project and replace these!
+// Logging
+#define CL_INFO(X)		std::cout << std::format("INFO: {}\n", x)
+#define CL_ERROR(x)		std::cout << std::format("ERROR: {}\n", x)
+
+#ifdef _DEBUG
+	#define CL_DEBUG(x) std::cout << std::format("DEBUG: {}\n", x)
+#else
+	#define CL_DEBUG(x)
 #endif
